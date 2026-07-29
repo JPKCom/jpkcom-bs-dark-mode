7ce99d0bee0f67dc2a1cddb87c02b967105af09b
Jean Pierre Kolb <jpk@jpkc.com>
2026-07-29 09:30:27 +0200

    v2.0.7: tested up to WordPress 7.1, minimum 6.9 throughout
    - Plugin header and README now declare Tested up to: 7.1
    - The bundled updater bailed out below WordPress 6.8 while the plugin header has
      required 6.9 for several releases, so the check could never fire on a supported
      installation. Both now say 6.9.
    - Release manifest fallbacks for requires/tested raised to 6.9 / 7.1

--- jpkcom-bs-dark-mode.php
+++ jpkcom-bs-dark-mode.php
@@ -3,15 +3,15 @@
 Plugin Name: JPKCom Bootstrap 5 Dark Mode Switch
 Plugin URI: https://github.com/JPKCom/jpkcom-bs-dark-mode
 Description: Shortcode [jpkcom-bs-dark-mode] and JS for Bootstrap 5 Dark Mode Switch.
-Version: 2.0.6
+Version: 2.0.7
 Author: Jean Pierre Kolb <jpk@jpkc.com>
 Author URI: https://www.jpkc.com
 Contributors: JPKCom
 Tags: Bootstrap, Color, Theme, Shortcode, Gutenberg
 Requires at least: 6.9
-Tested up to: 7.0
+Tested up to: 7.1
 Requires PHP: 8.3
-Stable tag: 2.0.6
+Stable tag: 2.0.7
 License: GPL-2.0-or-later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 */
@@ -29,7 +29,7 @@ if ( ! defined( constant_name: 'WPINC' ) ) {
  * @since 2.0.3
  */
 if ( ! defined( 'JPKCOM_BS_DARK_MODE_VERSION' ) ) {
-    define( 'JPKCOM_BS_DARK_MODE_VERSION', '2.0.6' );
+    define( 'JPKCOM_BS_DARK_MODE_VERSION', '2.0.7' );
 }
 
 
